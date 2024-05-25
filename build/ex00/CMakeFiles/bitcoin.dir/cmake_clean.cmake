494daab6f82bb92bd30803bfab031c3eadf1a088
rmitache <rmitache@student.42heilbronn.de>
2024-05-25 09:27:02 +0000

    first commit

---
+++ build/ex00/CMakeFiles/bitcoin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/workspaces/nix-dev/CPP-Module-09/ex00/bitcoin"
+  "/workspaces/nix-dev/CPP-Module-09/ex00/bitcoin.pdb"
+  "CMakeFiles/bitcoin.dir/main.cpp.o"
+  "CMakeFiles/bitcoin.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bitcoin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
