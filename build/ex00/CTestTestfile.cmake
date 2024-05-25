df7076d9e54f62a303fa0d7832d3d3bf512c9407
rmitache <rmitache@student.42heilbronn.de>
2024-05-25 10:14:27 +0000

    fixed?

--- build/ex00/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/nix-dev/CPP-Module-09/ex00
-# Build directory: /workspaces/nix-dev/CPP-Module-09/build/ex00
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
